15535a9a55ad48e93b2f8c6d57cbfdc7002dc6db
Natalia <natalia21@gmail.com>
2023-04-11 10:43:41 -0700

    spelling

--- Week-1/script.js
+++ Week-1/script.js
@@ -1,8 +1,8 @@
-const firstName= "Natalia";
-const lastName= "Quezada";
-const Study="6 months";
-const Intrest= "pentesting and ethical hacking";
+const firstName= 'Natalia';
+const lastName= 'Quezada';
+const Study='6 months';
+const Intrest= 'pentesting and ethical hacking';
 
-const introduction = "My name is ${firstName} ${lastName}. I have been at SCC for ${Study} and move into ${Intrest}";
+const introduction = `My name is ${firstName} ${lastName}. I have been at SCC for ${Study} and move into ${Intrest}`;
 
 document.getElementById("introduction").innerHTML= introduction;
