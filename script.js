11a2d338348562c20cd0e5a78337fdfd01d32ab7
Natalia <natalia21@gmail.com>
2023-04-11 10:32:41 -0700

    fixed?

--- script.js
+++
@@ -1,8 +0,0 @@
-const firstname= "Natalia";
-const lastname= "Quezada";
-const study="6 months";
-const intrest= "pentesting and ethical hacking";
-
-const introduction = My name is ${firstName} ${lastName}. I have been at SCC for ${study} and move into ${intrest};
-
-document.getElementById("introduction").innerHTML= introduction;
