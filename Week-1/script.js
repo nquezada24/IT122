3ef438db370c5efc297ecf842d4eea952d92088e
Natalia <natalia21@gmail.com>
2023-04-11 09:56:00 -0700

    changng things

--- Week-1/script.js
+++
@@ -1,9 +0,0 @@
-let firstname= "Natalia";
-let lastname= "Q.Evans";
-let Study="6 months";
-let goal="graduate";
-let intrest= "pentesting and ethical hacking";
-
-let intro = [firstname,lastname,Study,goal,intrest];
-
-document.getElementById("intro").innerHTML= intro;
