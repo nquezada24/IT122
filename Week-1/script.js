7651585faeed39ea6a3ad049cb9e7d37c113e53c
Natalia <natalia21@gmail.com>
2023-04-10 20:17:52 -0700

    new java

--- Week-1/script.js
+++ Week-1/script.js
@@ -1,4 +1,9 @@
-let firstName= 'Natalia';
-let lastName='Q.Evans';
-let Study='6 months';
-let goal='graduate'; 
+let firstname= "Natalia";
+let lastname= "Q.Evans";
+let Study="6 months";
+let goal="graduate";
+let intrest= "pentesting and ethical hacking";
+
+let intro = My Name is, (firstname) (lastname). I have, (Study) at SCC and my (goal), than move into (intrest);
+
+document.getElementById("intro").innerHTML= intro;
